36134de44a444e20e896ca6f837d6087deee63ac
AAAlex-123 <alexmandelias@gmail.com>
2021-11-03 18:52:32 +0200

    add helper files

---
+++ LexerTest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import minipython.lexer.Lexer;
+import minipython.node.Token;
+
+public class LexerTest1 {
+	public static void main(String[] args) 	{
+		try {
+			Lexer lexer = new Lexer(
+					new PushbackReader(
+					new FileReader(args[0].toString()), 1024));
+
+			Token token = lexer.next();
+			while (!token.getText().equals("")) {
+				// don't print blanks
+				if (token.getClass() != minipython.node.TBlank.class)
+					System.out.printf("%s:%s%n", token, token.getClass());
+
+				token = lexer.next(); 
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+}
