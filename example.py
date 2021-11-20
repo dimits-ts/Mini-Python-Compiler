b1e37b2025e73c780810000d47c292daf92c1f0a
AAAlex-123 <alexmandelias@gmail.com>
2021-11-20 12:39:51 +0200

    draft example file

--- example.py
+++ example.py
@@ -1,13 +1,26 @@
 #! /usr/bin/env python
 #A miniPython example
 
-def fib(n):    # write Fibonacci series up to n
-               a = 0
-               b = 1
-               while a < n:
-                        print a
-                        a = b
-                        b = a + b
-                      
-def funcwithdef(name,university="aueb"):
-       print name, " studies in ", university
+from a_module.submodule import member1 as name
+import another_module.submodule.member2
+
+def func(param1, param2 = 3):
+    jordan = 9 + 8 * 7**5
+    return jordan
+
+print "papaionaou", "ua" , "sebeste"
+
+print len(huwuawei.tonia(ccp + 8 ** 456))
+
+while true:
+    for id in ids:
+        id -= 2
+        if a > 2:
+            if b <= icantwritetests:
+                if is_gay(b) < is_gay(c):
+                    print uwu
+                    assert max(identifier.function_call(arg1, arg2), 67, "lmao")
+                
+
+
+
