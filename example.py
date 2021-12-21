937836030f5624900dbaf54d757c71274a55ebef
AAAlex-123 <alexmandelias@gmail.com>
2021-12-21 22:31:27 +0200

    slightly improve test

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
-#! /usr/bin/env python
-#A miniPython example
+# A miniPython example
+
+a = 1 + 2 + 3 + 4 + 5 + 6
 
 import a_module.submodule as name1, a_module2.submodule2 as name2
 from another_module import member1 as name3, member2 as name4
 
 def func(param1, param2 = 3, param3 = "hello"):
-    int1 = 1 + 2 * 3 - 4 ** 5 / 6 + 7 % 8
+    int1 = 1 + 2 * 3 - 4 ** 5 / 6 + 9 ** 7 % 8
     if not int1 > 5 and param3 == "hello":
-        int1 -= (1 + 2) * 3 - 4 ** (5 / (6 + 7)) % 8
-    int2 = len(max(1, 'a', "b")) + id2[id1.func1(arg1, arg2)]
+        int1 -= 1 + 2 * 3 - 4 ** 5 / 6 + 7 % 8
+    int2 = len(max(1, 'a', "b")) * id2 [id1.func1(arg1, arg2)]
     assert int1, int2
     func(param1, param2 + 5, ["hi", 'hello'])
     print "the string 'compilers' has length", len("compilers")
